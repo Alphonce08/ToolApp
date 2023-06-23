6f61a460a3ab514bcf07fd1a38442eb88420be3f
Alphonce <alphoncerotich30@gmail.com>
2023-06-23 17:09:37 +0300

    Expenses list

---
+++ app/src/main/java/com/example/balancetoolapp/ExpensesActivity.kt
@@ -0,0 +1,20 @@
+package com.example.balancetoolapp
+
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.icu.util.Calendar
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.DatePicker
+
+class ExpensesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_expenses)
+
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/balancetoolapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.balancetoolapp
+
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var BtnExpenses: Button
+    lateinit var BtnReceived: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        BtnExpenses = findViewById(R.id.mBExpenses)
+        BtnReceived = findViewById(R.id.mBReceived)
+        BtnExpenses.setOnClickListener {
+            startActivity(Intent(applicationContext,ExpensesActivity::class.java))
+        }
+        BtnReceived.setOnClickListener {
+            startActivity(Intent(applicationContext,ReceivedActivity::class.java))
+        }
+
+
+
+    }
+}
