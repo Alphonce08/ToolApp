7a028eea76b18c0f24b02144177fdfe3428e3bec
Alphonce <alphoncerotich30@gmail.com>
2023-06-27 17:25:31 +0300

    email and password

--- app/src/main/java/com/example/balancetoolapp/Login.kt
+++ app/src/main/java/com/example/balancetoolapp/Login.kt
@@ -1,17 +1,24 @@
 package com.example.balancetoolapp
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.NonCancellable.message
 
 class Login : AppCompatActivity() {
     lateinit var EdtEmail:EditText
     lateinit var EdtPassword:EditText
     lateinit var TxtRegister:TextView
     lateinit var BtLogin:Button
+    lateinit var progress:ProgressDialog
+    lateinit var mAuth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -26,6 +33,43 @@ class Login : AppCompatActivity() {
 
         TxtRegister.setOnClickListener {
             startActivity(Intent(this, Register::class.java))
+        }
+        mAuth = FirebaseAuth.getInstance()
+        progress = ProgressDialog(this)
+        progress.setTitle("Loading")
+        progress.setMessage("Please wait...")
+        BtLogin.setOnClickListener {
+
+            //start by receiving data from the user
+            var email = EdtEmail.text.toString().trim()
+            var password = EdtPassword.text.toString().toString()
+            //check if the user is submitting empty fields
+            if (email.isEmpty()){
+                EdtEmail.setError("Please fill this input")
+                EdtEmail.requestFocus()
+            } else if (password.isEmpty()){
+                EdtPassword.setError("Please fill this input")
+                EdtPassword.requestFocus()
+            } else {
+                //Proceed to Login the user
+                progress.show()
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    progress.dismiss()
+                    if (it.isComplete){
+                        Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+
+                        finish()
+
+                    } else {
+                        displayMessage("ERROR", it.exception!!.message.toString())
+                        var alertDialog = AlertDialog.Builder(this)
+                        alertDialog.setTitle(title)
+                        alertDialog.setMessage(message)
+                        alertDialog.setPositiveButton("Ok", null)
+                        alertDialog.create().show()
+                    }
+                }
+            }
         }
         }
 
--- app/src/main/java/com/example/balancetoolapp/Register.kt
+++ app/src/main/java/com/example/balancetoolapp/Register.kt
@@ -1,30 +1,70 @@
 package com.example.balancetoolapp
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class Register : AppCompatActivity() {
     lateinit var EditEmail:EditText
     lateinit var EdtPassword:EditText
     lateinit var BtRegister:Button
+    lateinit var progress:ProgressDialog
+    lateinit var mAuth:FirebaseAuth
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
-        EditEmail = findViewById(R.id.mEdtEmail)
-        EdtPassword = findViewById(R.id.mEdtPassword)
-        BtRegister = findViewById(R.id.BtnRegister)
-        BtRegister.setOnClickListener {
-            startActivity(Intent(applicationContext, MainActivity::class.java))
 
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_register)
+            EditEmail = findViewById(R.id.mEdtEmail)
+            EdtPassword = findViewById(R.id.mEdtPassword)
+            BtRegister = findViewById(R.id.BtnRegister)
+            BtRegister.setOnClickListener {
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+
+            }
+            mAuth = FirebaseAuth.getInstance()
+            progress = ProgressDialog(this)
+            progress.setTitle("Loading")
+            progress.setMessage("Please wait...")
+            BtRegister.setOnClickListener {
+                //start by receiving data from the user
+                var email = EditEmail.text.toString().trim()
+                var password = EdtPassword.text.toString().toString()
+                //check if the user is submitting empty fields
+                if (email.isEmpty()) {
+                    EditEmail.setError("Please fill this input")
+                    EditEmail.requestFocus()
+                } else if (password.isEmpty()) {
+                    EdtPassword.setError("Please fill this input")
+                    EdtPassword.requestFocus()
+                } else if (password.length < 6) {
+                    EdtPassword.setError("Password is too short")
+                    EdtPassword.requestFocus()
+                } else {
+                    //Proceed to register the user
+                    progress.show()
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                        progress.dismiss()
+                        if (it.isComplete) {
+                            Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT)
+                                .show()
+                            mAuth.signOut()
+
+                            finish()
+
+
+                        }
+                    }
+                }
+            }
         }
-
-
-
-
-    }
 }
+
+
+
